8f3c5de960417e3385ed14ea11a7429c9fe8ade7
Alexander Bock <alex.bock@rice.edu>
2024-04-25 14:27:32 -0500

    network graph construction

--- grn_eval/network.py
+++ grn_eval/network.py
@@ -1,6 +1,7 @@
 
 from __future__ import annotations
 
+import networkx as nx
 import pandas as pd
 
 
@@ -11,10 +12,18 @@ class Network:
         return
 
     @classmethod
-    def from_df(cls, df: pd.DataFrame) -> Network:
+    def from_df(
+        cls, df: pd.DataFrame, u_col: str, v_col: str, edge_weight_col: str,
+        edge_weight_threshold: float = None
+    ) -> Network:
 
         network = cls()
+        if edge_weight_threshold is not None:
+            df = df[df[edge_weight_col] > edge_weight_threshold]
         network.df = df
+        network.graph = nx.from_pandas_edgelist(
+            df, source=u_col, target=v_col, edge_attr=edge_weight_col
+        )
 
         return network
 
